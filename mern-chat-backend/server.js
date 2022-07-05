347dc4f6ef3d96078fa6d510f4e4e0fe0a5f9f60
mert-ozturk <38337566+mert-ozturk@users.noreply.github.com>
2022-07-05 14:34:42 +0300

    test2

---
+++ mern-chat-backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express()
+const rooms = ['general','tech','finance','crypto'];
+
+
+//Middlewares 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.use(cors());
+
+
+const server = require('http').createServer(app)
+const PORT = 5001;
+const io = require('socket.io')(server,{
+    cors:{
+        origin:'http//localhost:3000',
+        methods:['GET','POST']
+    }
+})
+
+server.listen(PORT,()=>{
+    console.log('Listening to port',PORT)
+})
